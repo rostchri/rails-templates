423c4c33604ffcf1c67245cfb1d466ce84d44101
Christian Rost <chr@baltic-online.de>
2013-05-22 12:24:40 +0200

    Fix

--- kickstrap.rb
+++ kickstrap.rb
@@ -93,19 +93,19 @@ file 'app/views/kaminari/_next_page.html.haml', <<-CODE
     = link_to_unless current_page.last?, railstrap_image('icon-forward'), url, :rel => 'next', :remote => remote
 CODE
 
-# 
-# file 'app/views/kaminari/_page.html.haml', <<-CODE
-# -#  Link showing page number
-# -#  available local variables
-# -#    page:          a page object for "this" page
-# -#    url:           url to this page
-# -#    current_page:  a page object for the currently displayed page
-# -#    num_pages:     total number of pages
-# -#    per_page:      number of items to fetch per page
-# -#    remote:        data-remote
-# %li{:class=>"page #{' active' if page.current? }"}
-#   = link_to page, url, opts = {:remote => remote, :rel => page.next? ? 'next' : page.prev? ? 'prev' : nil}
-# CODE
+
+file 'app/views/kaminari/_page.html.haml', <<-CODE
+-#  Link showing page number
+-#  available local variables
+-#    page:          a page object for "this" page
+-#    url:           url to this page
+-#    current_page:  a page object for the currently displayed page
+-#    num_pages:     total number of pages
+-#    per_page:      number of items to fetch per page
+-#    remote:        data-remote
+%li{:class=>"page \#{' active' if page.current? }"}
+  = link_to page, url, opts = {:remote => remote, :rel => page.next? ? 'next' : page.prev? ? 'prev' : nil}
+CODE
 
 
 file 'app/views/kaminari/_paginator.html.haml', <<-CODE
@@ -129,22 +129,22 @@ file 'app/views/kaminari/_paginator.html.haml', <<-CODE
       = next_page_tag unless current_page.last? 
       = last_page_tag unless current_page.last?
 = raw(t('views.pagination.items',:low => collection.offset_value + 1, :high => collection.offset_value + collection.length, :total => collection.total_count))
-= "Einträge #{collection.offset_value + 1} - #{collection.offset_value + collection.length} von insgesamt #{collection.total_count}"
+= "Einträge \#{collection.offset_value + 1} - \#{collection.offset_value + collection.length} von insgesamt \#{collection.total_count}"
 CODE
-# 
-# 
-# file 'app/views/kaminari/_prev_page.html.haml', <<-CODE
-# -#  Link to the "Previous" page
-# -#  available local variables
-# -#    url:           url to the previous page
-# -#    current_page:  a page object for the currently displayed page
-# -#    num_pages:     total number of pages
-# -#    per_page:      number of items to fetch per page
-# -#    remote:        data-remote
-# - unless current_page.first?
-#   %li{:class=>"prev"}
-#     = link_to_unless current_page.first?, railstrap_image('icon-backward'), url, :rel => 'prev', :remote => remote
-# CODE
+
+
+file 'app/views/kaminari/_prev_page.html.haml', <<-CODE
+-#  Link to the "Previous" page
+-#  available local variables
+-#    url:           url to the previous page
+-#    current_page:  a page object for the currently displayed page
+-#    num_pages:     total number of pages
+-#    per_page:      number of items to fetch per page
+-#    remote:        data-remote
+- unless current_page.first?
+  %li{:class=>"prev"}
+    = link_to_unless current_page.first?, railstrap_image('icon-backward'), url, :rel => 'prev', :remote => remote
+CODE
 
 
 
